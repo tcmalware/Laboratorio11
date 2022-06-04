1f398ac495352e0be014bb207944c366814107b6
tcmalware <genarodvo@gmail.com>
2022-06-03 19:36:26 -0500

    Agregar archivos de proyecto.

---
+++ MVVM/MVVM/Views/Operations.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+namespace MVVM.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Operations : ContentPage
+    {
+        public Operations()
+        {
+            InitializeComponent();
+            this.BindingContext = new ViewModels.OperationsViewModel();
+        }
+    }
+}
